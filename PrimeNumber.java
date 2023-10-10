0165178c5ee07f77a1193bb14ee3ce06438c4fe5
KrithikaDanesh <147545175+KrithikaDanesh@users.noreply.github.com>
2023-10-10 14:14:50 -0500

    Add files via upload

---
+++ PrimeNumber.java
@@ -0,0 +1,26 @@
+package week1.assignments;
+
+public class PrimeNumber {
+	public static void main(String[] args) {
+		 int i,m=0;
+		 boolean flag=false;
+		  int n=3;//it is the number to be checked    
+		  m=n/2;      
+		  if(n==0||n==1){  
+		   System.out.println(n+" is not prime number");      
+		  }else{  
+		   for(i=2;i<=m;i++){      
+		    if(n%i==0){      
+		     System.out.println(n+" is not prime number");      
+		     flag=true;      
+		     break;      
+		    }      
+		   }      
+		   if(flag==false)  { 
+			   System.out.println(n+" is prime number"); 
+			   }  
+		  } 
+		
+	}
+
+}
