387d7ca91b6fa1213902f9954fd2209ac067e4bd
KrithikaDanesh <147545175+KrithikaDanesh@users.noreply.github.com>
2023-10-11 13:06:25 -0500

    Update PrimeNumber.java

--- Day2/PrimeNumber.java
+++ Day2/PrimeNumber.java
@@ -4,7 +4,7 @@ public class PrimeNumber {
 	public static void main(String[] args) {
 		 int i,m=0;
 		 boolean flag=false;
-		  int num=13;//it is the number to be checked    
+		  int num=13; 
 		  m=num/2;      
 		  if(num==0||num==1){  
 		   System.out.println(num+" is not a prime number");      
