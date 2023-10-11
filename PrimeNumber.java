d8c028001f57d07148aeffb0d8bd17f6785418eb
KrithikaDanesh <147545175+KrithikaDanesh@users.noreply.github.com>
2023-10-10 21:07:10 -0500

    Update PrimeNumber.java

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -4,20 +4,20 @@ public class PrimeNumber {
 	public static void main(String[] args) {
 		 int i,m=0;
 		 boolean flag=false;
-		  int n=3;//it is the number to be checked    
+		  int n=13;//it is the number to be checked    
 		  m=n/2;      
 		  if(n==0||n==1){  
-		   System.out.println(n+" is not prime number");      
+		   System.out.println(n+" is not a prime number");      
 		  }else{  
 		   for(i=2;i<=m;i++){      
 		    if(n%i==0){      
-		     System.out.println(n+" is not prime number");      
+		     System.out.println(n+" is not a prime number");      
 		     flag=true;      
 		     break;      
 		    }      
 		   }      
 		   if(flag==false)  { 
-			   System.out.println(n+" is prime number"); 
+			   System.out.println(n+" is a prime number"); 
 			   }  
 		  } 
 		
