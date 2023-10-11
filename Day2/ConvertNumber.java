d2ab074eec359743e26abb0dde5e3e6fc85fb953
KrithikaDanesh <147545175+KrithikaDanesh@users.noreply.github.com>
2023-10-11 11:38:20 -0500

    Add files via upload

---
+++ Day2/ConvertNumber.java
@@ -0,0 +1,17 @@
+package week1.assignments;
+
+public class ConvertNumber {
+
+	public static void main(String[] args) {
+		
+		int num = -40;
+		
+		if (num < 0 )
+			System.out.println("Its an negative number");
+			System.out.println("The number -40 is converted to"+" " + -(num));
+	
+
+	}
+	
+
+}
---
+++ Day2/Palindrome.java
@@ -0,0 +1,25 @@
+package week1.assignments;
+
+public class Palindrome {
+	public static void main(String[] args) {
+		int num = 35453;
+		int a , sum ,lastDigit ;
+		a= num;
+		//Reverse Number
+		for (sum=0;a>0;a/=10) {
+			//System.out.println("Input Number" + ""+a);
+			lastDigit=a%10;
+			sum=(sum*10)+lastDigit;
+	
+		}
+		if(sum==num)
+			System.out.println("Its a Palindrome Number");
+		else
+			System.out.println("Its not a Palindrome Number");
+		
+	}
+	
+	
+
+	
+}
